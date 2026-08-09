a0b1e1cc0ef4edcb88f3938672d663af873afd52
mossishahi <ge65cew@mytum.de>
2026-08-09 13:51:56 +0200

    release: v0.2.16

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -62,7 +62,7 @@ import {
   type ClaudeAgentConnectionValidator,
 } from "./settings.js";
 
-const VERSION = "0.2.15";
+const VERSION = "0.2.16";
 const SNAPSHOT_THROTTLE_MS = 500;
 const HEARTBEAT_MS = 15_000;
 const POLL_MS = 2_000;
